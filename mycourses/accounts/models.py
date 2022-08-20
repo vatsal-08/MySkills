7f6a6ce16b5fe9b1fd8a9b5690e56f121dfc9719
Vatsal Khatri <201951081@iiitvadodara.ac.in>
2022-08-20 23:54:47 +0530

    Made sme changes in the User models

--- mycourses/accounts/models.py
+++ mycourses/accounts/models.py
@@ -4,8 +4,9 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth.models import User
 
-# class CustomUser(User):
-#     email
-#     username
-#     date_joined
-#     last_login
+
+class CustomUser(User):
+    email = models.EmailField(max_length=255, unique=True)
+    username = models.TextField(null=False, blank=True)
+    date_joined = models.DateTimeField(auto_now=True, blank=True)
+    last_login = models.DateTimeField(auto_now=True, blank=True)
