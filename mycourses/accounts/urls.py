f72a5120ad336a622a38b6f36c31f677cbb31ce2
Vatsal Khatri <201951081@iiitvadodara.ac.in>
2023-04-02 18:02:33 +0530

    Erased all

--- mycourses/accounts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from accounts.views import * 
-urlpatterns = [
-    path('signup/', SignUpView.as_view(template_name='accounts/signup.html'), name='signup'),
-    path('login/', LoggingView.as_view(), name='login'),
-    path('logout/', logout, name='logout'),
-]
