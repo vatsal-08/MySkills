fcbfeb56fb732bbe92de8e24332b411436bf4fd1
Vatsal Khatri <201951081@iiitvadodara.ac.in>
2023-04-11 16:14:37 +0530

    Made few more changes

--- myskills/courses/urls.py
+++ myskills/courses/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('',index,name='home'),
     path('courses/',CourseList.as_view(),name='courses'),
     path('courses/<int:pk>/',CourseList.as_view(),name='courses'),
-    path('about-me/',views.about,name='courses'),
+    path('about-me/',views.about,name='about-me'),
 ]
