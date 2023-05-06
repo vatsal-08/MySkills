f92ddf1b3a50482efdd3ffb19fedf711af984cd6
Vatsal Khatri <201951081@iiitvadodara.ac.in>
2023-05-06 23:56:22 +0530

    Email auth functions

--- myskills/accounts/views.py
+++ myskills/accounts/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+import uuid
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.conf import settings
+from .models import *
+from django.core.mail import send_mail
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
 def login(request):
     return render(request,'accounts/login.html')
 
 def register(request):
     return render(request,'accounts/signup.html')
+def success(request):
+    return render(request,'accounts/success.html')
+
+def token_send(request):
+    return render(request,'accounts/token_send.html')
