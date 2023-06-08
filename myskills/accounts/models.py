02368321022a3d8cecebf28428a1bcfbf4cbd04e
Vatsal Khatri <201951081@iiitvadodara.ac.in>
2023-06-08 12:44:21 +0530

    Fixed the model for user

--- myskills/accounts/models.py
+++ myskills/accounts/models.py
@@ -1,15 +1,50 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-class CustomUser(AbstractUser):
+class UserManager(BaseUserManager):
+    def create_user(self, email, first_name, last_name, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, first_name=first_name, last_name=last_name, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, first_name, last_name, password=None, **extra_fields):
+        extra_fields.setdefault('is_admin', True)
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_verified', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, first_name, last_name, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser):
     email = models.EmailField(primary_key=True)
     first_name = models.CharField(max_length=150)
     last_name = models.CharField(max_length=150)
     is_verified = models.BooleanField(default=False)
-    auth_token = models.CharField(max_length=255)
+    auth_token = models.CharField(max_length=255, default='')
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    joining_date = models.DateTimeField(auto_now_add=True)
+
+    objects = UserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name']
 
     def __str__(self):
         return self.email
+
+    # Add the necessary methods required by AbstractBaseUser
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return self.is_admin
+    
