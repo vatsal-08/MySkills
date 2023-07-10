5962c25bc4cf3dba74c38c3a0c5bd38c44b84186
Vatsal Khatri <201951081@iiitvadodara.ac.in>
2023-07-10 23:44:35 +0530

    Solved problems with update button button

--- myskills/courses/views.py
+++ myskills/courses/views.py
@@ -3,8 +3,8 @@ from .models import Course
 from django.shortcuts import get_object_or_404
 import os
 from django.db.models import Q
-from django.http import JsonResponse
-from django.template.loader import render_to_string
+from django.contrib import messages
+from django.urls import reverse
 
 def delete_file(file_path):
     if os.path.exists(file_path):
@@ -39,28 +39,26 @@ def about(request):
 def list_view(request):
     courses = Course.objects.all()
     context = {"courses": courses}
-    context["add_course"] = {"button_url": "", "classes": ""}
-    if request.user.is_authenticated:
-        if request.user.is_superuser:
-            context["add_course"] = {
-                "button_url": "create-view",
-                "classes": "fa-solid fa-circle-plus",
-            }
+    create_view_url = reverse('create-view')
+    add_course_button = f'<a href="{create_view_url}"><i class="fa-solid fa-circle-plus"></i></a>'
+    if not request.user.is_authenticated or not request.user.is_superuser:
+        add_course_button=""
+    context["add_course_button"]=add_course_button
     return render(request, 'courses/home.html', context)
 
 
 def course_detail(request, pk):
-    course = get_object_or_404(Course, pk=pk)
-    context = {'course': course}
-    return render(request, 'courses/detail.html', context)
-
-def superuser_check(user):
-    return user.is_superuser
+    try:
+        course = Course.objects.get(pk=pk)
+        context = {'course': course}
+        return render(request, 'courses/detail.html', context)
+    except Course.DoesNotExist:
+        messages.error(request, "Course does not exist.")
+        return redirect('courses')
 
 def create_view(request):
-    if not request.user.is_authenticated:
-        return redirect('courses')
-    if not request.user.is_superuser:
+    if not request.user.is_authenticated or not request.user.is_superuser:
+        messages.error(request,"You are not authorised")
         return redirect('courses')
     if request.method == 'POST':
         name = request.POST.get('name')
@@ -75,65 +73,74 @@ def create_view(request):
     return render(request, "courses/new_courses_add.html")
 
 def update_course(request, pk):
-    course = get_object_or_404(Course, pk=pk)
-    img_file_name = course.img.name.split("/")[-1]
-    pdf_file_name = course.pdf_file.name.split("/")[-1]
+    if not request.user.is_authenticated or not request.user.is_superuser:
+        messages.error(request,"You are not authorised")
+        return redirect('courses')
+    try:
+        course = Course.objects.get(pk=pk)
+        img_file_name = course.img.name.split("/")[-1]
+        pdf_file_name = course.pdf_file.name.split("/")[-1]
 
-    if request.method == 'POST':
-        action = request.POST.get('update-button')
+        if request.method == 'POST':
+            action = request.POST.get('update-button')
+            if action == "cancel":
+                return redirect('courses')
+            name = request.POST.get('name')
+            cost = request.POST.get('cost')
+            description = request.POST.get('description')
+            course.name = name
+            course.cost = cost
+            course.description = description
+            if 'uploadimg' in request.FILES:
+                try:
+                    uploaded_img = request.FILES['uploadimg']
 
-        if action == "cancel":
+                    if course.img:
+                        delete_file(course.img.path)
+
+                    course.img = uploaded_img
+                except Exception as e:
+                    message=f"Error uploading image: {str(e)}"
+                    messages.error(request,message)
+                    return redirect('courses')
+
+            if 'pdfupload' in request.FILES:
+                try:
+                    uploaded_pdf = request.FILES['pdfupload']
+
+                    if course.pdf_file:
+                        delete_file(course.pdf_file.path)
+
+                    course.pdf_file = uploaded_pdf
+                except Exception as e:
+                    message=f"Error uploading PDF: {str(e)}"
+                    messages.error(request,message)
+                    return redirect('courses')
+
+            course.save()
+            messages.success(request,"Successfully updated the course")
             return redirect('courses')
 
-        name = request.POST.get('name')
-        cost = request.POST.get('cost')
-        description = request.POST.get('description')
+        pdf_file_name, extension1 = os.path.splitext(pdf_file_name)
+        pdf_file_name = truncate_text(pdf_file_name, 15) + extension1
+        
+        img_file_name, extension2 = os.path.splitext(img_file_name)
+        img_file_name = truncate_text(img_file_name, 15) + extension2
 
-        course.name = name
-        course.cost = cost
-        course.description = description
-
-        if 'uploadimg' in request.FILES:
-            try:
-                uploaded_img = request.FILES['uploadimg']
-
-                if course.img:
-                    delete_file(course.img.path)
-
-                course.img = uploaded_img
-            except Exception as e:
-                print(f"Error uploading image: {str(e)}")
-
-        if 'pdfupload' in request.FILES:
-            try:
-                uploaded_pdf = request.FILES['pdfupload']
-
-                if course.pdf_file:
-                    delete_file(course.pdf_file.path)
-
-                course.pdf_file = uploaded_pdf
-            except Exception as e:
-                print(f"Error uploading PDF: {str(e)}")
-
-        course.save()
-
-        return redirect('courses')
-
-    pdf_file_name, extension1 = os.path.splitext(pdf_file_name)
-    pdf_file_name = truncate_text(pdf_file_name, 15) + extension1
-    
-    img_file_name, extension2 = os.path.splitext(img_file_name)
-    img_file_name = truncate_text(img_file_name, 15) + extension2
-
-    context = {
-        "course": course,
-        "pdf_file_name": pdf_file_name,
-        "img_file_name": img_file_name
-    }
-
-    return render(request, 'courses/course_update.html', context)
+        context = {
+            "course": course,
+            "pdf_file_name": pdf_file_name,
+            "img_file_name": img_file_name
+        }
+        return render(request, 'courses/course_update.html', context)
+    except Course.DoesNotExist:
+        messages.error(request,"Invalid link")
+        return redirect("courses")
 
 def delete_course(request, pk):
+    if not request.user.is_authenticated or not request.user.is_superuser:
+        messages.error(request,"You are not authorised")
+        return redirect('courses')
     course = get_object_or_404(Course, pk=pk)
 
     if request.method == 'POST':
